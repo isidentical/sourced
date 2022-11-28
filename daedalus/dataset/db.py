18441ee4ee145b809d1cdf529671d26040a9ca74
Batuhan Taskaya <isidentical@gmail.com>
2022-11-29 01:03:11 +0300

    feat: add a global DB

--- daedalus/dataset/db.py
+++ daedalus/dataset/db.py
@@ -8,7 +8,20 @@ from enum import Enum
 from pathlib import Path
 from typing import Any
 
-DAEDALUS_CACHE_FILE = "dataset.json"
+from platformdirs import user_config_dir
+
+GLOBAL_CONFIG_VERSION = "0.0.1"
+
+DAEDALUS_GLOBAL_DIR = Path(
+    user_config_dir(
+        "daedalus",
+        "daedalus",
+        version=GLOBAL_CONFIG_VERSION,
+    )
+)
+DAEDALUS_GLOBAL_DIR.mkdir(parents=True, exist_ok=True)
+
+DEFAULT_DATASET_CACHE_FILE_NAME = "datasets.json"
 JSONType = dict[str, Any]
 
 
@@ -20,7 +33,7 @@ class Dataset:
 
     @classmethod
     def from_cache(cls, cache_dir: Path) -> Dataset:
-        meta_path = cache_dir / DAEDALUS_CACHE_FILE
+        meta_path = cache_dir / DEFAULT_DATASET_CACHE_FILE_NAME
         if not meta_path.exists():
             raise FileNotFoundError(
                 f"Dataset metadata file ({meta_path}) was not found."
@@ -32,25 +45,37 @@ class Dataset:
     def cache(self) -> None:
         """Cache the dataset metadata."""
 
-        meta_path = self.path / DAEDALUS_CACHE_FILE
+        meta_path = self.path / DEFAULT_DATASET_CACHE_FILE_NAME
         temp_fd, temp_file = tempfile.mkstemp(suffix=meta_path.suffix, dir=self.path)
         with open(temp_fd, "w") as stream:
             json.dump(self.to_json(), stream)
         os.replace(temp_file, meta_path)
 
     def to_json(self) -> JSONType:
+        # When serializing, use paths relative to the path of
+        # this dataset.
+
         return {
             "name": self.name,
             "path": str(self.path),
-            "sources": [source.to_json() for source in self.sources],
+            "sources": [
+                source.to_json(
+                    relative_to=self.path,
+                )
+                for source in self.sources
+            ],
         }
 
     @classmethod
     def from_json(cls, json_data: JSONType) -> Dataset:
+        dataset_path = Path(json_data["path"])
         return cls(
             name=json_data["name"],
-            path=Path(json_data["path"]),
-            sources=[Source.from_json(source) for source in json_data["sources"]],
+            path=dataset_path,
+            sources=[
+                Source.from_json(source, relative_to=dataset_path)
+                for source in json_data["sources"]
+            ],
         )
 
 
@@ -67,16 +92,81 @@ class Source:
     status: SourceStatus = SourceStatus.AWAITING_DOWNLOAD
 
     @classmethod
-    def from_json(cls, json_data: JSONType) -> Source:
+    def from_json(cls, json_data: JSONType, relative_to: Path) -> Source:
+        if json_data["path"] is not None:
+            path = relative_to / json_data["path"]
+        else:
+            path = None
+
         return cls(
             name=json_data["name"],
-            path=Path(json_data["path"]) if json_data["path"] is not None else None,
+            path=path,
             status=SourceStatus(json_data["status"]),
         )
 
-    def to_json(self) -> JSONType:
+    def to_json(self, relative_to: Path) -> JSONType:
+        if self.path is not None:
+            path = str(self.path.relative_to(relative_to))
+        else:
+            path = None
+
         return {
             "name": self.name,
-            "path": str(self.path) if self.path is not None else None,
+            "path": path,
             "status": self.status.name,
         }
+
+
+@dataclass
+class GlobalStore:
+    datasets: dict[str, Dataset] = field(default_factory=dict)
+    path: Path = DAEDALUS_GLOBAL_DIR / "store.json"
+
+    @classmethod
+    def from_file(cls, path: Path) -> GlobalStore:
+        if not path.exists():
+            return cls(path=path)
+
+        with open(path) as stream:
+            return cls.from_json(json.load(stream), path=path)
+
+    @classmethod
+    def from_json(cls, json_data: JSONType, path: Path) -> GlobalStore:
+        datasets = {}
+        for dataset in json_data["datasets"]:
+            name = dataset["name"]
+            metadata_file = Path(dataset["path"])
+            try:
+                dataset = Dataset.from_cache(metadata_file)
+            except FileNotFoundError:
+                print(
+                    f"WARNING: Dataset '{name}' does not appear to be exist on the disk"
+                    f" (no {metadata_file}). Skipping."
+                )
+            else:
+                datasets[name] = dataset
+
+        return cls(datasets, path=path)
+
+    def to_json(self) -> JSONType:
+        # Only include references to the actual dataset metadata
+        # files, not the full dataset metadata.
+        return {
+            "datasets": [
+                {"name": dataset.name, "path": str(dataset.path)}
+                for dataset in self.datasets.values()
+            ],
+        }
+
+    def cache(self) -> None:
+        """Cache the global store metadata."""
+
+        temp_fd, temp_file = tempfile.mkstemp(
+            suffix=self.path.suffix, dir=self.path.parent
+        )
+        with open(temp_fd, "w") as stream:
+            json.dump(self.to_json(), stream)
+        os.replace(temp_file, self.path)
+
+
+load = Dataset.from_cache
--- tests/dataset/test_db.py
+++ tests/dataset/test_db.py
@@ -2,34 +2,117 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from daedalus.dataset import db
+from daedalus.dataset.db import (
+    DEFAULT_DATASET_CACHE_FILE_NAME,
+    Dataset,
+    GlobalStore,
+    Source,
+)
 
 
 def test_cache(tmp_path):
     cache_dir = Path(tmp_path) / "cache"
     cache_dir.mkdir(parents=True)
 
-    dataset = db.Dataset(
+    dataset = Dataset(
         name="test",
         path=cache_dir,
         sources=[
-            db.Source(name="source1", path=cache_dir / "source1"),
-            db.Source(name="source2", path=cache_dir / "source2"),
+            Source(name="source1", path=cache_dir / "source1"),
+            Source(name="source2", path=cache_dir / "source2"),
         ],
     )
     dataset.cache()
 
-    assert (cache_dir / db.DAEDALUS_CACHE_FILE).exists()
-    cached_dataset = db.Dataset.from_cache(cache_dir)
+    assert (cache_dir / DEFAULT_DATASET_CACHE_FILE_NAME).exists()
+    cached_dataset = Dataset.from_cache(cache_dir)
 
     assert dataset.name == cached_dataset.name
     assert dataset.path == cached_dataset.path
     assert dataset.sources == cached_dataset.sources
 
-    dataset.sources.append(db.Source(name="source3", path=cache_dir / "source3"))
+    dataset.sources.append(Source(name="source3", path=cache_dir / "source3"))
     dataset.cache()
 
-    cached_dataset = db.Dataset.from_cache(cache_dir)
+    cached_dataset = Dataset.from_cache(cache_dir)
     assert dataset.name == cached_dataset.name
     assert dataset.path == cached_dataset.path
     assert len(cached_dataset.sources)
+
+
+def test_global_store_non_existent(tmp_path):
+    global_path = tmp_path / "global"
+    global_path.mkdir(parents=True)
+
+    global_store_path = global_path / "datasets.json"
+    store = GlobalStore.from_file(global_store_path)
+    assert store.datasets == {}
+
+    assert not global_store_path.exists()
+
+    store.cache()
+    assert global_store_path.exists()
+
+    store = GlobalStore.from_file(global_store_path)
+    assert store.datasets == {}
+
+
+def test_global_store(tmp_path):
+    pypi_path = tmp_path / "pypi"
+    pypi_path.mkdir(parents=True)
+
+    dataset_pypi = Dataset(
+        name="pypi",
+        path=pypi_path,
+        sources=[
+            Source(name="pypi", path=pypi_path / "pypi"),
+        ],
+    )
+
+    global_path = tmp_path / "global"
+    global_path.mkdir(parents=True)
+
+    global_store_path = global_path / "datasets.json"
+    store = GlobalStore(path=global_store_path)
+    store.cache()
+
+    # This dataset does not exist yet
+    store = GlobalStore.from_file(path=global_store_path)
+    assert store.datasets == {}
+
+    # Create the dataset
+    store.datasets["pypi"] = dataset_pypi
+    dataset_pypi.cache()
+    store.cache()
+
+    store = GlobalStore.from_file(global_store_path)
+    assert store.datasets == {"pypi": dataset_pypi}
+
+    # Add a new dataset
+    conda_path = tmp_path / "conda"
+    conda_path.mkdir(parents=True)
+
+    dataset_conda = Dataset(
+        name="conda",
+        path=conda_path,
+        sources=[
+            Source(name="conda", path=conda_path / "conda"),
+        ],
+    )
+    dataset_conda.cache()
+
+    store = GlobalStore.from_file(global_store_path)
+    assert store.datasets == {"pypi": dataset_pypi}
+
+    store.datasets["conda"] = dataset_conda
+    store.cache()
+
+    store = GlobalStore.from_file(global_store_path)
+    assert store.datasets == {"pypi": dataset_pypi, "conda": dataset_conda}
+
+    # Remove a dataset
+    del store.datasets["conda"]
+    store.cache()
+
+    store = GlobalStore.from_file(global_store_path)
+    assert store.datasets == {"pypi": dataset_pypi}
